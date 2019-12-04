d91580387f8dff3febb81f53c3884a0a13a5765e
Victor <fed.viktor178@gmail.com>
2019-12-05 00:12:01 +0300

    Прикрутил еще немного бантиков

--- src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
+++ src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
@@ -1,25 +1,17 @@
 package ru.geekbrains.java_one.lesson_a.online;
 
 public class HelloWorld {
-    private static void primer() {  // Первое задание
-        int a = 5;
-        int b = 6;
-        int c = 7;
-        int d = 2;
+    private static void primer(int a, int b, int c, int d) {  // Первое задание
         System.out.println(a * (b + c / (d * 1f)));
     }
-    public static void checkRange() { // Второе задание
-        int first = 9;
-        int second = 10;
-
+    public static void checkRange(int first, int second) { // Второе задание
         if ((first + second) >= 10 && (first + second) <= 20) {
             System.out.println("True");
         } else {
             System.out.println("False");
         }
     }
-    public static void whatNumber() { // Третье задание
-        int check = 56;
+    public static void whatNumber(int check) { // Третье задание
         if (check < 0) {
             System.out.println("Вы ввели отрицательное число");
         }
@@ -27,12 +19,10 @@ public class HelloWorld {
             System.out.println("Вы ввели положительное число");
         }
     }
-    public static void gritings(){ // Четвёртое задание
-        String name = ("Victor");
+    public static void gritings(String name){ // Четвёртое задание
         System.out.println("Приветствуем Вас, " + name);
     }
-    public static void whatYear(){ // Пятое задание
-        int year = 1;
+    public static void whatYear(int year){ // Пятое задание
         if ((year % 400) == 0){
             System.out.println("Вы ввели " + year + " год, он является високосным");
         }
@@ -47,10 +37,10 @@ public class HelloWorld {
         }
     }
     public static void main(String[] args) {
-        primer();
-        checkRange();
-        whatNumber();
-        gritings();
-        whatYear();
+        primer(3,8,7,8);
+        checkRange(9, 12);
+        whatNumber(55);
+        gritings("Viktor");
+        whatYear(56);
     }
 }
