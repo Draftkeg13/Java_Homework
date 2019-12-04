3822776dcbede2e5effa2279059a04b84fa53cd5
Victor <fed.viktor178@gmail.com>
2019-12-04 22:43:28 +0300

    Закончил оформление первого задания

--- src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
+++ src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
@@ -2,12 +2,13 @@ package ru.geekbrains.java_one.lesson_a.online;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {}
+    public static void primer(){  // Первое задание
             int a = 5;
             int b = 6;
             int c = 7;
             int d = 2;
             System.out.println(a * (b + c /( d * 1f)));
-
     }
+
 }
