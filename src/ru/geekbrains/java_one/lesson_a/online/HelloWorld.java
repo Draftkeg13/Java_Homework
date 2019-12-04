191a0e2d51b366e719eca249017c02927f35393e
Victor <fed.viktor178@gmail.com>
2019-12-04 23:12:36 +0300

    Вот теперь точно закончил оформление первого задания и выполнил второе задание

--- src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
+++ src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
@@ -1,14 +1,25 @@
 package ru.geekbrains.java_one.lesson_a.online;
 
 public class HelloWorld {
-
-    public static void main(String[] args) {}
-    public static void primer(){  // Первое задание
-            int a = 5;
-            int b = 6;
-            int c = 7;
-            int d = 2;
-            System.out.println(a * (b + c /( d * 1f)));
+    private static void primer() {  // Первое задание
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int d = 2;
+        System.out.println(a * (b + c / (d * 1f)));
     }
+    public static void checkrange() { // Второе задание
+        int first = 9;
+        int second = 10;
 
+        if ((first + second) >= 10 && (first + second) <= 20) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+    public static void main(String[] args) {
+        primer();
+        checkrange();
+    }
 }
