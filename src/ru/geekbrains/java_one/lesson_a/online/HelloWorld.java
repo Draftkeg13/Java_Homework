e4cc65bbbfe9ce1efd3dc93a8e88395bacb9dbc4
Victor <fed.viktor178@gmail.com>
2019-12-05 00:02:21 +0300

    Пятое задание

--- src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
+++ src/ru/geekbrains/java_one/lesson_a/online/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         int d = 2;
         System.out.println(a * (b + c / (d * 1f)));
     }
-    public static void checkrange() { // Второе задание
+    public static void checkRange() { // Второе задание
         int first = 9;
         int second = 10;
 
@@ -18,7 +18,7 @@ public class HelloWorld {
             System.out.println("False");
         }
     }
-    public static void whatnumber() { // Третье задание
+    public static void whatNumber() { // Третье задание
         int check = 56;
         if (check < 0) {
             System.out.println("Вы ввели отрицательное число");
@@ -31,10 +31,26 @@ public class HelloWorld {
         String name = ("Victor");
         System.out.println("Приветствуем Вас, " + name);
     }
+    public static void whatYear(){ // Пятое задание
+        int year = 1;
+        if ((year % 400) == 0){
+            System.out.println("Вы ввели " + year + " год, он является високосным");
+        }
+        else if ((year % 100) == 0){
+            System.out.println("Вы ввели " + year + " год, он не является високосным");
+        }
+        else if ((year % 4) == 0){
+            System.out.println("Вы ввели " + year + " год, он является високосным");
+        }
+        else {
+            System.out.println("Вы ввели " + year + " год, он не является високосным");
+        }
+    }
     public static void main(String[] args) {
         primer();
-        checkrange();
-        whatnumber();
+        checkRange();
+        whatNumber();
         gritings();
+        whatYear();
     }
 }
