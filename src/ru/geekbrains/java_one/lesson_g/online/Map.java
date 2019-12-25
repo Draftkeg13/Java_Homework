4b8217f08411b366508a952cc267aff39ddf9399
Victor <fed.viktor178@gmail.com>
2019-12-25 23:29:37 +0300

    Оригинал

---
+++ src/ru/geekbrains/java_one/lesson_g/online/Map.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.java_one.lesson_g.online;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+    public static final int MODE_HVH = 0;
+    public static final int MODE_HVA = 1;
+
+    Map() {
+        setBackground(Color.BLUE);
+    }
+
+    void startNewGame(int gameMode, int fieldSizeX, int fieldSizeY, int winLength) {
+        System.out.printf("mode = %d\nfsX = %d\nwin = %d", gameMode, fieldSizeX, winLength);
+    }
+}
